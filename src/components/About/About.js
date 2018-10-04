7f61da2792eb6b61957da7fa4d59f6a38856fe25
Horacio Hernandez <hhernandez@rv-clt-hhernandez.redventures.net>
2018-10-04 15:26:11 -0400

    added sticky nav

---
+++ src/components/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <img src="../../../static/img/horaciodrums.jpg" alt="Horacio"/>
+            <div className="about-container__content">
+                <h2>About me</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel diam facilisis, elementum eros vitae, laoreet erat.
+                     Nulla et elit finibus, laoreet massa ac, tincidunt lectus. Curabitur felis lacus, euismod eu cursus a, posuere in purus. 
+                     Phasellus mattis nisl a massa mollis, eget malesuada purus venenatis. Fusce suscipit risus eu auctor ultricies. Etiam porta sit amet 
+                     neque nec sollicitudin. Nullam tincidunt porttitor sem in accumsan. Nulla id quam vitae quam mollis malesuada. Proin risus ex, placerat a 
+                     vestibulum sed, ornare et justo. Quisque nec nibh 
+                    fringilla lorem pharetra suscipit. Phasellus scelerisque ipsum dolor.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,32 @@ import React from "react"
 
 
 const Header = () => {
+
+    
+
+// Get the navbar
+
+
+// Get the offset position of the navbar
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+const stickyNavBar = ()  => {
+    const navbar = document.getElementById("main-nav");
+    const sticky = navbar.offsetTop;
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+window.onscroll = () => stickyNavBar();
+
+
     return (
     <header>
 
-        <nav className="main-nav">
+        <nav className="main-nav" id="main-nav">
         <p>hhh</p>
             <ul>
                 <li><a href='#'>About</a></li>
