e6c5bd8363430b11b80b3689998d20dc73f66676
Horacio Hernandez <hhernandez@rv-clt-hhernandez.redventures.net>
2018-10-06 15:42:54 -0400

    Updates

---
+++ precache-manifest.5314b7e8ca5857c9ce8ce5d43eb66a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4699102a5fac9eb944b0",
+    "url": "/personalPortfolio/static/js/runtime~main.4699102a.js"
+  },
+  {
+    "revision": "ce2b313c711414a38ef0",
+    "url": "/personalPortfolio/static/js/main.ce2b313c.chunk.js"
+  },
+  {
+    "revision": "7411921a118e10710502",
+    "url": "/personalPortfolio/static/js/1.7411921a.chunk.js"
+  },
+  {
+    "revision": "ce2b313c711414a38ef0",
+    "url": "/personalPortfolio/static/css/main.3e593ba8.chunk.css"
+  },
+  {
+    "revision": "33bcf8b26c9e9ee3203c3797bdd48a1c",
+    "url": "/personalPortfolio/index.html"
+  }
+];
