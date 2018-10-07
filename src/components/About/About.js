b4d09c554b56cb7d56f7e1e21b91a65d3ba756ef
Horacio Hernandez <hhernandez@rv-clt-hhernandez.redventures.net>
2018-10-06 23:14:34 -0400

    added mobile nav

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,9 +7,9 @@ const About = () => {
             <img src="https://i.imgur.com/qMUK2Hm.png" alt="Horacio"/>
             <div className="about-container__content">
                 <h2>About me</h2>
-                <p>I was born in Michoàcan, Mexico. I came to the United States at the age of 5. Growing up, I faced a lot of struggles, from learning a whole a
-                     whole new language. I made a lot of mistakes in the past, but it as made me a better person. I never gave up on achieving my career goals and I 
-                     can not wait to see what it hold for me. </p>
+                <p>I was born in Michoàcan, Mexico. I came to the United States at the age of 5. Growing up, I faced a lot of struggles, from learning a whole 
+                      new language. I made a lot of mistakes in the past, but it as made me a better person and I have learned from those experiences. I never gave up on achieving my career goals and I 
+                     can not wait to see what the future holds for me. </p>
                     <p> I was apart of the Road to Hire tech program. A non-profit organization that helps young adults achieve their goals regardless of their background.
                         Throughtout the program, I have not only learned coding languages but, also soft skills. I'm excited to continue learning and my future as a web 
                         developer.
