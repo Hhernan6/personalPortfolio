48e123c42b4412d1156c18e04b8c66fcbb16d5f9
Horacio Hernandez <hhernandez@rv-clt-hhernandez.redventures.net>
2018-10-06 23:14:59 -0400

    Updates

--- precache-manifest.1e9c6d05f1931639f1dbc50579e2fac0.js
+++ precache-manifest.1e9c6d05f1931639f1dbc50579e2fac0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/personalPortfolio/static/js/runtime~main.4699102a.js"
   },
   {
-    "revision": "8184f4fcbb6322e7c6e4",
-    "url": "/personalPortfolio/static/js/main.8184f4fc.chunk.js"
+    "revision": "69adf33dd1040257ec11",
+    "url": "/personalPortfolio/static/js/main.69adf33d.chunk.js"
   },
   {
     "revision": "7411921a118e10710502",
     "url": "/personalPortfolio/static/js/1.7411921a.chunk.js"
   },
   {
-    "revision": "8184f4fcbb6322e7c6e4",
-    "url": "/personalPortfolio/static/css/main.8ce33b7a.chunk.css"
+    "revision": "69adf33dd1040257ec11",
+    "url": "/personalPortfolio/static/css/main.f97ea6eb.chunk.css"
   },
   {
-    "revision": "cfc8fbdfa5b0c463299da38b1c018f03",
+    "revision": "2f9d912d68ae604f0a46902ae4bf169b",
     "url": "/personalPortfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/personalPortfolio/precache-manifest.ab3c5b135fe060e174567baaf954b8dc.js"
+  "/personalPortfolio/precache-manifest.1e9c6d05f1931639f1dbc50579e2fac0.js"
 );
 
 workbox.clientsClaim();
