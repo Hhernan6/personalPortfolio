c119fdc2f33847a366a88849d8b5355780f132a1
Horacio Hernandez <hhernandez@rv-clt-hhernandez.redventures.net>
2018-10-06 21:31:29 -0400

    added social media nav

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const footer = () => {
     return (
-        <footer></footer>
+        <footer>
+            <nav className="social-media__nav">
+                <ul>
+                    <li><a href="http://facebook.com "className="social-media__nav__facebook-icon"></a></li>
+                    <li className="social-media__nav-link--margin-left "><a href="http://linkedin.com" className="social-media__nav__linkedIn-icon"></a></li>
+              
+          
+                </ul>
+            </nav>
+        </footer>
     )
 }
 
