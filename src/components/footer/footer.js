f3fbf1f15b1ef2519a8f17cbc3c4aed901d746b5
Horacio Hernandez <hhernandez@rv-clt-hhernandez.redventures.net>
2018-10-06 22:32:04 -0400

    styling changes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,8 +5,8 @@ const footer = () => {
         <footer>
             <nav className="social-media__nav">
                 <ul>
-                    <li><a href="http://facebook.com "className="social-media__nav__facebook-icon"></a></li>
-                    <li className="social-media__nav-link--margin-left "><a href="http://linkedin.com" className="social-media__nav__linkedIn-icon"></a></li>
+                    <li><a href="https://www.facebook.com/horacio.hernandez.5203" target="_blank"className="social-media__nav__facebook-icon"></a></li>
+                    <li className="social-media__nav-link--margin-left "><a target="_blank"href="https://www.linkedin.com/in/horacio-hernandez-39bb3813b/" className="social-media__nav__linkedIn-icon"></a></li>
               
           
                 </ul>
